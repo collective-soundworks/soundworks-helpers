7dd7d214d806a64e953bab2eb327f743b82a1b6e
b-ma <benjamin.matuszewski@gmail.com>
2024-06-04 10:08:49 +0200

    feat: provide single entry for each platform (node and browser)

---
+++ browser-client/browser.js
@@ -0,0 +1,7 @@
+/**
+ * Helpers for browser runtimes.
+ * @example
+ * import { loadConfig, launcher } from '@soundworks/helpers/node.js';
+ */
+export { default as launcher } from './launcher.js';
+export { default as loadConfig } from './load-config.js';
---
+++ node-client/node.js
@@ -0,0 +1,6 @@
+/**
+ * Helpers for node runtimes.
+ */
+export { default as launcher } from './launcher.js';
+export { default as loadConfig } from './load-config.js';
+
